43851c256c6ef0591b6d26e85a76b5d69f1deccd
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-24 16:58:42 -0500

    Implement IAlbumService

--- src/main/java/co/com/music/domain/service/IAlbumService.java
+++ src/main/java/co/com/music/domain/service/IAlbumService.java
@@ -1,15 +1,15 @@
 package co.com.music.domain.service;
 
-import co.com.music.domain.dto.album.CreateAlbumDTO;
-import co.com.music.domain.dto.album.UpdateAlbumDTO;
 import co.com.music.persistence.entities.Album;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Transactional
 public interface IAlbumService {
     Flux<Album> findAll();
     Mono<Album> findById(String id);
-    Mono<Album> save(CreateAlbumDTO albumDTO);
-    Mono<Album> update(UpdateAlbumDTO albumDTO);
+    Mono<Album> save(Album album);
+    Mono<Album> update(Album album);
     Mono<Album> delete(String id);
 }
--- src/main/java/co/com/music/domain/service/implementation/AlbumServiceImpl.java
+++ src/main/java/co/com/music/domain/service/implementation/AlbumServiceImpl.java
@@ -1,37 +1,51 @@
 package co.com.music.domain.service.implementation;
 
-import co.com.music.domain.dto.album.CreateAlbumDTO;
-import co.com.music.domain.dto.album.UpdateAlbumDTO;
+import co.com.music.infrastructure.exception.HttpException;
 import co.com.music.persistence.entities.Album;
 import co.com.music.domain.service.IAlbumService;
+import co.com.music.persistence.repository.IAlbumRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@AllArgsConstructor
 public class AlbumServiceImpl implements IAlbumService {
+    private final IAlbumRepository albumRepository;
+
     @Override
     public Flux<Album> findAll() {
-        return null;
+        return this.albumRepository.findAll();
     }
 
     @Override
     public Mono<Album> findById(String id) {
-        return null;
+        return this.albumRepository.findById(id)
+                .switchIfEmpty(
+                        Mono.error(
+                                HttpException.builder()
+                                        .message("The album with the given id cannot be found.")
+                                        .httpStatus(HttpStatus.NOT_FOUND)
+                                        .build()
+                        )
+                );
     }
 
     @Override
-    public Mono<Album> save(CreateAlbumDTO albumDTO) {
-        return null;
+    public Mono<Album> save(Album album) {
+        return this.albumRepository.save(album);
     }
 
     @Override
-    public Mono<Album> update(UpdateAlbumDTO albumDTO) {
-        return null;
+    public Mono<Album> update(Album album) {
+        return this.albumRepository.save(album);
     }
 
     @Override
     public Mono<Album> delete(String id) {
-        return null;
+        return this.findById(id)
+                .doOnNext(this.albumRepository::delete);
     }
 }
