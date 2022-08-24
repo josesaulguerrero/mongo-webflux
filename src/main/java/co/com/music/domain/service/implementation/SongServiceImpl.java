493090ac5ab37611d0731b199ca3f549394cfcd7
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-08-24 17:27:50 -0500

    Implement ISongService interface

---
+++ src/main/java/co/com/music/domain/service/implementation/SongServiceImpl.java
@@ -0,0 +1,60 @@
+package co.com.music.domain.service.implementation;
+
+import co.com.music.domain.service.ISongService;
+import co.com.music.infrastructure.exception.HttpException;
+import co.com.music.persistence.entities.Song;
+import co.com.music.persistence.repository.ISongRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Service
+@AllArgsConstructor
+public class SongServiceImpl implements ISongService {
+    private final ISongRepository songRepository;
+
+    @Override
+    public Flux<Song> findAll() {
+        return this.songRepository.findAll();
+    }
+
+    @Override
+    public Mono<Song> findById(String id) {
+        return this.songRepository.findById(id)
+                .switchIfEmpty(
+                        Mono.error(HttpException.builder()
+                                .message("The Song with the given id does not exist or cannot be found.")
+                                .httpStatus(HttpStatus.NOT_FOUND)
+                                .build())
+                );
+    }
+
+    @Override
+    public Mono<Song> save(Song song) {
+        song.setTimestamp(LocalDateTime.now());
+        return this.songRepository.save(song);
+    }
+
+    @Override
+    public Mono<Song> update(Song song) {
+        return this.findById(song.getSongId())
+                .map(songFromDB -> {
+                    songFromDB.setName(song.getName());
+                    songFromDB.setLyricsBy(song.getLyricsBy());
+                    songFromDB.setArrangedBy(song.getArrangedBy());
+                    songFromDB.setProducedBy(song.getProducedBy());
+                    return songFromDB;
+                })
+                .flatMap(this.songRepository::save);
+    }
+
+    @Override
+    public Mono<Song> delete(String id) {
+        return this.findById(id)
+                .doOnNext(this.songRepository::delete);
+    }
+}
